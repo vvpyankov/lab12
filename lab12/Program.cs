b5573eb0400c7adc19823abe3e0144e5027187b7
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-23 21:08:19 +0300

    23.01.2022

--- lab12/Program.cs
+++ lab12/Program.cs
@@ -6,69 +6,60 @@ using System.Threading.Tasks;
 
 namespace lab12
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            try
+            {
+                Console.Write("Для вычисления характеристик круга укажите его радиус: ");
+                double radius = Convert.ToDouble(Console.ReadLine());
+                if (radius <= 0)
+                {
+                    Console.Write("Радиус не может быть меньше 0 (включительно).");
+                    return;
+                }
+                Console.Write("Для определения принадлежности точки окружности укажите значение Х: ");
+                double coorX = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+                Console.Write("А теперь значение Y для точки: ");
+                double coorY = Math.Abs(Convert.ToDouble(Console.ReadLine()));
+                Circle.circLenght(radius);
+                Circle.circSquare(radius);
+                Circle.GetPosition(radius, coorX, coorY);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода.");
+                return;
+            }
+            Console.ReadKey();
+        }
+
+        static class Circle
+        {
+            public static void circLenght(double radius)
+            {
+                double Lenght = Math.PI * 2 * radius;
+                Console.WriteLine("Длина окружности равна {0:f2}", Lenght);
+            }
+            public static void circSquare(double radius)
+            {
+                double Square = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine("Площадь круга равна {0:f2}", Square);
+            }
+            public static void GetPosition(double radius, double coorX, double coorY)
+            {
+                if (coorX <= radius && coorY <= radius)
+                {
+                    Console.WriteLine("Поздравляю! Точка ({0},{1}) принадлежит кругу с радиусом {2}", coorX, coorY, radius);
+                }
+                else
+                {
+                    Console.WriteLine("Мимо! Точка ({0},{1}) не принадлежит кругу с радиусом {2}", coorX, coorY, radius);
+                }
+            }
         }
     }
 }
-using System;
-					
-public class Program
-{
-	public static void Main()
-	{
-		/*1.   Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:
-		метод, определяющий длину окружности по заданному радиусу;
-		метод, определяющий площадь круга по заданному радиусу;
-		метод, проверяющий принадлежность точки с координатами (x,y) кругу с радиусом r и координатами центра x0, y0.*/
-		try
-		{
-			Console.Write("Введите радиус окружности: ");
-			double Radius = Convert.ToDouble(Console.ReadLine());
-			if (Radius <= 0)
-			{
-				Console.Write("Радиус должен быть задан действительным числом.");
-				return;
-			}
-			Console.Write("Введите координату Х точки: ");
-			double positionX = Math.Abs(Convert.ToDouble(Console.ReadLine()));
-			Console.Write("Введите координату Y точки: ");
-			double positionY = Math.Abs(Convert.ToDouble(Console.ReadLine()));
-			Circle.GetСircumference(Radius);
-			Circle.GetSquare(Radius);
-			Circle.GetPosition(Radius, positionX, positionY);
-		}
-		catch (FormatException)
-		{
-			Console.WriteLine("Неверный формат ввода.");
-			return;
-		}
-	}
 
-	static class Circle
-	{
-		public static void GetСircumference(double Radius)
-		{
-			double Сircumference = Math.PI * 2 * Radius;
-			Console.WriteLine("Длина окружности равна {0:f2}", Сircumference);
-		}
-		public static void GetSquare(double Radius)
-		{
-			double Square = Math.PI * Math.Pow(Radius, 2.0);
-			Console.WriteLine("Площадь круга равна {0:f2}", Square);
-		}
-		public static void GetPosition(double Radius, double positionX, double positionY)
-		{
-			if (positionX <= Radius && positionY <= Radius)
-			{
-				Console.WriteLine("Точка с координатами ({0},{1}) принадлежит кругу с радиусом {2}", positionX, positionY, Radius);
-			}
-			else
-			{
-				Console.WriteLine("Точка с координатами ({0},{1}) не принадлежит кругу с радиусом {2}", positionX, positionY, Radius);
-			}
-		}
-	}
-}
+
